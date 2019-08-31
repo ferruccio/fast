79704c89510d00770a9ce6c33cb063ba28c4509c
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2019-08-31 08:24:52 -0400

    Do not modify the "default" profile
    It turns out that setting the session token on the default profile is a bad idea.
    It works the first time it's updated, when there's no existing session token.
    After that we get a "The security token included in the request is invalid." error every time.
    
    Now the profile parameter is no longer optional and it must be set to
    something other than "default".

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use clap::{App, AppSettings, Arg};
 use ini::Ini;
 use rusoto_core::{region::Region, CredentialsError};
 use rusoto_sts::{GetSessionTokenError, GetSessionTokenRequest, Sts, StsClient};
+use std::str;
 
 quick_error! {
     #[derive(Debug)]
@@ -61,32 +62,43 @@ fn main() -> Result<()> {
                 .value_name("PROFILE")
                 .help("AWS profile to update")
                 .index(2)
-                .default_value("default")
-                .required(false),
+                .required(true),
         )
         .get_matches();
 
     let credentials = app.value_of("credentials").unwrap_or("");
     let profile = Some(app.value_of("profile").unwrap_or(""));
+    if profile.unwrap() == "default" {
+        println!("cannot set default profile");
+        return Ok(());
+    }
 
     let sts = StsClient::new(Region::default());
 
     let req = GetSessionTokenRequest {
         ..Default::default()
     };
-    let rsp = sts.get_session_token(req).sync()?;
-
-    if let Some(cred) = rsp.credentials {
-        let mut conf = Ini::load_from_file(credentials)?;
-        conf.set_to(profile, "aws_access_key_id".to_owned(), cred.access_key_id);
-        conf.set_to(
-            profile,
-            "aws_secret_access_key".to_owned(),
-            cred.secret_access_key,
-        );
-        conf.set_to(profile, "aws_session_token".to_owned(), cred.session_token);
-        conf.write_to_file(credentials)?;
-        println!("session token expires: {}", cred.expiration);
+    match sts.get_session_token(req).sync() {
+        Err(err) => match err {
+            GetSessionTokenError::Unknown(unknown) => {
+                println!("unknown error: {}", str::from_utf8(&unknown.body).unwrap());
+            }
+            _ => println!("{:?}", err),
+        },
+        Ok(rsp) => {
+            if let Some(cred) = rsp.credentials {
+                let mut conf = Ini::load_from_file(credentials)?;
+                conf.set_to(profile, "aws_access_key_id".to_owned(), cred.access_key_id);
+                conf.set_to(
+                    profile,
+                    "aws_secret_access_key".to_owned(),
+                    cred.secret_access_key,
+                );
+                conf.set_to(profile, "aws_session_token".to_owned(), cred.session_token);
+                conf.write_to_file(credentials)?;
+                println!("session token expires: {}", cred.expiration);
+            }
+        }
     }
 
     Ok(())
